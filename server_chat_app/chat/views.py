c1bc4920bdc4b2de5ef68d2cb3317fe362a35199
aniket panchal <aniketpanchal07@gmail.com>
2024-10-11 18:11:19 +0530

    added more error handling  in view

--- server_chat_app/chat/views.py
+++ server_chat_app/chat/views.py
@@ -1,3 +1,4 @@
+# chat/views.py
 from django.http import HttpResponse
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
@@ -6,8 +7,11 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 from .models import ChatRoom, Message
 from .serializers import UserSerializer, ChatRoomSerializer, MessageSerializer
-
 from django.contrib.auth.models import User
+from rest_framework.exceptions import ValidationError
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.permissions import IsAuthenticated
+from django.db.utils import IntegrityError
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -18,27 +22,69 @@ class UserViewSet(viewsets.ModelViewSet):
     def login(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
+
+        if not username or not password:
+            return Response(
+                {"error": "Both username and password are required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         user = authenticate(username=username, password=password)
         if user:
             token, _ = Token.objects.get_or_create(user=user)
             return Response({"token": token.key})
         return Response(
-            {"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST
+            {"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
         )
+
+    def create(self, request, *args, **kwargs):
+        try:
+            return super().create(request, *args, **kwargs)
+        except IntegrityError:
+            return Response(
+                {"error": "A user with that username already exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
 
 class ChatRoomViewSet(viewsets.ModelViewSet):
     queryset = ChatRoom.objects.all()
     serializer_class = ChatRoomSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        try:
+            serializer.save()
+        except ValidationError as e:
+            raise ValidationError(detail=str(e))
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super().destroy(request, *args, **kwargs)
+        except ObjectDoesNotExist:
+            return Response(
+                {"error": "ChatRoom not found"}, status=status.HTTP_404_NOT_FOUND
+            )
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        try:
+            serializer.save(user=self.request.user)
+        except ValidationError as e:
+            raise ValidationError(detail=str(e))
+
+    def list(self, request, *args, **kwargs):
+        room_id = request.query_params.get("room")
+        if room_id:
+            self.queryset = self.queryset.filter(room_id=room_id)
+        return super().list(request, *args, **kwargs)
 
 
 def hello_world(request):
-    return HttpResponse("api in running")
+    return HttpResponse("API is running")
+
